23f063018a68cb44653af05b3f1960b35bcef085
Jason Cross <code@hellojason.net@>
2022-01-17 23:48:30 -0600

    blog and hero styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,14 +6,14 @@ module.exports = {
 		colors: {
 			// Tint generator: https://tailwind-color-palette.netlify.app/
 			'primary': {
-				0        : '#EDD6FD', 
-				100      : '#DBADFB', 
-				200      : '#C884F8', 
-				300      : '#B65BF6', 
-				400      : '#A432F4', 
-				500      : '#8328C3', 
-				600      : '#621E92', 
-				700      : '#421462', 
+				0        : '#EDD6FD',
+				100      : '#DBADFB',
+				200      : '#C884F8',
+				300      : '#B65BF6',
+				400      : '#A432F4',
+				500      : '#8328C3',
+				600      : '#621E92',
+				700      : '#421462',
 				800      : '#210A31'
 			},
 			'secondary': '#FAAC2C',
