5eaceaa6b9058361c217b528d7e6490f3e510695
Jason Cross <code@hellojason.net@>
2022-01-17 21:49:12 -0600

    article styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,17 @@ module.exports = {
 			'sans': ['Montserrat', 'Segoe UI', 'SegoeUI', 'Helvetica Neue', 'Helvetica', 'Arial', 'sans-serif'],
 		},
 		colors: {
-			'primary': '#A432F4',
+			'primary': {
+				0: '#EDD6FD', 
+				100: '#DBADFB', 
+				200: '#C884F8', 
+				300: '#B65BF6', 
+				400: '#A432F4', 
+				500: '#8328C3', 
+				600: '#621E92', 
+				700: '#421462', 
+				800: '#210A31'
+			},
 			'secondary': '#FAAC2C',
 			'white': '#FFFFFF',
 			'black': '#2F3021'
